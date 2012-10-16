3b21b4bdebf1ba538e6d66d7f75ecd9f494c527c
Vadim Rutkovsky <roignac@gmail.com>
2012-10-16 16:06:47 +0300

    Другие категории

--- lib/pages/categories/photo/other_page.rb
+++ lib/pages/categories/photo/other_page.rb
@@ -9,3 +9,17 @@ class CategoryPhotoOtherPage < AdDetailsPage
   irr_multi_select "Тип предложения", "offertype"
   irr_multi_select "Состояние", "used-or-new"
 end
+
+class CategoryPhotoPhototechnicsPage < AdDetailsPage
+  include PageObject
+
+  @@url_suffix = "/photo/phototechnics"
+  @category = "Фото, оптика -> Фототехника"
+end
+
+class CategoryPhotoOpticsPage < AdDetailsPage
+  include PageObject
+
+  @@url_suffix = "/photo/optics"
+  @category = "Фото, оптика -> Оптика"
+end
