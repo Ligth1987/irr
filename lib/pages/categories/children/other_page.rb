ee91505158116eced66d7d103459d673d291a667
Vadim Rutkovsky <roignac@gmail.com>
2012-10-16 13:21:24 +0300

    Товары для детей

--- lib/pages/categories/children/other_page.rb
+++ lib/pages/categories/children/other_page.rb
@@ -9,3 +9,17 @@ class CategoryChildrenOtherPage < AdDetailsPage
   irr_multi_select "Тип предложения", "offertype"
   irr_multi_select "Состояние", "used-or-new"
 end
+
+class CategoryChildrenBabyFurniturePage < AdDetailsPage
+  include PageObject
+
+  @@url_suffix = "/children/baby-furniture"
+  @category = "Товары для детей -> Детская мебель"
+end
+
+class CategoryChildrenBabyWarePage < AdDetailsPage
+  include PageObject
+
+  @@url_suffix = "/children/babyware"
+  @category = "Товары для детей -> Товары для малышей"
+end
